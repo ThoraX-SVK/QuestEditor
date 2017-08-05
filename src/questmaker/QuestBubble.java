2046f18b35855724ed5acfbc47464da53b0afbd3
ThoraX-SVK <krason.tomas@gmail.com>
2017-08-05 10:57:59 +0200

    graficke upravy

--- src/questmaker/QuestBubble.java
+++ src/questmaker/QuestBubble.java
@@ -3,10 +3,6 @@ package questmaker;
 import java.awt.Color;
 import java.awt.Point;
 
-/**
- *
- * @author Tom
- */
 public class QuestBubble {
     
     String questName;
@@ -14,7 +10,6 @@ public class QuestBubble {
     int posY;
     int bubbleSize;
     Color bubbleColor;
-    
     Quest quest;
 
     public QuestBubble(String questName,int posX, int posY, Color bubbleColor, Quest quest) {
@@ -30,16 +25,10 @@ public class QuestBubble {
         this.quest.addOutput();
         this.quest.addOutput();
         this.quest.addOutput();
-        
     }
     
     public boolean MouseOverlaps(Point mousePosition) {
         return mousePosition.x > posX && mousePosition.x < posX+bubbleSize &&
                mousePosition.y > posY && mousePosition.y < posY+20; // 20 -> Nahardkodene v paintbuffer() v QuestMainFrameDraw
-    }
-
-    
-    
-    
-    
+    } 
 }
